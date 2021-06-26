11b6a2f1774bee433e198cadfe14ebd8183c889c
Lenon Diedrich <lenon.diedrich@gmail.com>
2021-06-26 16:06:49 -0300

    Game component created and Data fetch, HomePage started

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,9 +1,18 @@
 import styles from './styles.module.scss';
 
+var formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+})
+
 export default function Game({id, name, price, score, image}) {
     return(
-        <div className={styles.gameContainer}>
-
+        <div>
+            <h3>{name}</h3>
+            <img src={image} alt="Game"/>
+            <div className={styles.gameDetails}>
+            <span>{formatter.format(price)}</span>
+            </div>
         </div>
     )
 }
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,16 +1,54 @@
-import styles from './styles.module.scss';
-import Game from '../Game/index';
+import styles from "./styles.module.scss";
+import { useState, useEffect } from "react";
+import api from "../../services/api";
+import Game from "../Game";
 
 export default function HomePage() {
-    return (
-        <div className={styles.homeContainer}>
-            <Game
-            id="123"
-            name="Call of duty"
-            price={12.90}
-            score={100}
-            image="http://google.com.br"
-            />
-        </div>
-    )
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getAllGames();
+  }, []);
+
+  const getAllGames = async () => {
+    await api
+      .get("games")
+      .then((response) => {
+        const allGames = response.data;
+        setGames(allGames);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <div className={styles.sortProducts}>
+        <label>
+          Ordenar por:
+          <select>
+            <img src="/arrow-down-icon.svg" alt="Ordenar por" />
+            <option value="Preço">Preço</option>
+            <option value="Popularidade">Popularidade</option>
+            <option value="Alfabética">Alfabética</option>
+          </select>
+        </label>
+      </div>
+      <section className={styles.gamesList}>
+        <ul>
+          {games.map((game) => {
+            return (
+              <li key={game.id}>
+                <Game
+                  name={game.name}
+                  image={game.image}
+                  price={game.price}
+                  score={game.score}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
+  );
 }
